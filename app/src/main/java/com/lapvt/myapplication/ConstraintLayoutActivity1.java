a3d5ed18586fb10d8845c182fab28be01af955dd
LapVT2k3 <kendyvu2k3@gmail.com>
2023-07-27 10:31:52 +0700

    btvn_b2

---
+++ app/src/main/java/com/lapvt/myapplication/ConstraintLayoutActivity1.java
@@ -0,0 +1,14 @@
+package com.lapvt.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintLayoutActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout1);
+    }
+}
