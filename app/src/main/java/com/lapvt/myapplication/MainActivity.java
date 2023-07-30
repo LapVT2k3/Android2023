08655eba0487ecb1c0999046c332dbb920ff7b47
LapVT2k3 <kendyvu2k3@gmail.com>
2023-07-30 15:52:07 +0700

    btvn_b2

--- app/src/main/java/com/lapvt/myapplication/MainActivity.java
+++ app/src/main/java/com/lapvt/myapplication/MainActivity.java
@@ -3,14 +3,21 @@ package com.lapvt.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnClickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnClickMe = findViewById(R.id.btnClickMe);
 
+        btnClickMe.setOnClickListener(view -> {
+
+        });
     }
 }
