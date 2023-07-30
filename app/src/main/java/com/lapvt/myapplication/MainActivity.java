5a9d9174748814a0de6abd28c4d9216e153309c1
LapVT2k3 <kendyvu2k3@gmail.com>
2023-07-30 08:23:54 +0700

    btvn_b2

--- app/src/main/java/com/lapvt/myapplication/MainActivity.java
+++ app/src/main/java/com/lapvt/myapplication/MainActivity.java
@@ -3,35 +3,13 @@ package com.lapvt.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    //Khi tạo biến trong activity thì chúng ta nên để private
-    private Button btnClickMe;
-    private TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Quy tắc đặt tên hàm:
-//        TinhGiaTri(); -> Sai
-//        tinhGiaTri(); -> Đúng
 
-        btnClickMe = findViewById(R.id.btnClickMe);
-
-        btnClickMe.setOnClickListener(view -> debug());
-    }
-
-    private void debug() {
-        int result = 0;
-        for (int i = 1; i <= 20; i++) {
-            result = result + i;
-        }
-        Log.d("alo", result + "");
-        Log.e("alo", result + "");
     }
 }
