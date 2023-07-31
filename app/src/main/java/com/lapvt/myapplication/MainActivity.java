d1244fd5f56dad2214c5469227270886db693ed9
LapVT2k3 <kendyvu2k3@gmail.com>
2023-07-31 11:09:19 +0700

    b3

--- app/src/main/java/com/lapvt/myapplication/MainActivity.java
+++ app/src/main/java/com/lapvt/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.lapvt.myapplication.recyclerview.RecyclerViewActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnClickMe;
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         btnClickMe = findViewById(R.id.btnClickMe);
 
         btnClickMe.setOnClickListener(view -> {
-            Intent intent = new Intent(this, B3Activity.class);
+            Intent intent = new Intent(this, RecyclerViewActivity.class);
             startActivity(intent);
         });
     }
