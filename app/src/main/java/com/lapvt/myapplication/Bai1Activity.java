4d4354f6c04f1492be84eb471f81db0644bbc7e2
LapVT2k3 <kendyvu2k3@gmail.com>
2023-07-31 23:19:58 +0700

    b3

--- app/src/main/java/com/lapvt/myapplication/Bai1Activity.java
+++ app/src/main/java/com/lapvt/myapplication/Bai1Activity.java
@@ -3,12 +3,34 @@ package com.lapvt.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Bai1Activity extends AppCompatActivity {
+
+    private TextView tvBack;
+    private EditText edtAccount;
+    private EditText edtPassword;
+    private EditText edtPasswordAgain;
+    private Button btnContinue;
+    private TextView tvNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bai1);
+
+        tvBack = findViewById(R.id.tvBack);
+        edtAccount = findViewById(R.id.edtAccount);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtPasswordAgain = findViewById(R.id.edtPasswordAgain);
+        btnContinue = findViewById(R.id.btnContinue);
+        tvNotification = findViewById(R.id.tvNotification);
+
+        tvBack.setOnClickListener(view -> {
+//            Toast.makeText()
+        });
     }
 }
