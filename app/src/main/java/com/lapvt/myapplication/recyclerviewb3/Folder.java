660c117a682383f4e15b33578f32a453ea9fd443
LapVT2k3 <kendyvu2k3@gmail.com>
2023-08-05 11:39:27 +0700

    b4

---
+++ app/src/main/java/com/lapvt/myapplication/recyclerviewb3/Folder.java
@@ -0,0 +1,17 @@
+package com.lapvt.myapplication.recyclerviewb3;
+
+public class Folder {
+    private String nameFolder;
+
+    public Folder(String nameFolder) {
+        this.nameFolder = nameFolder;
+    }
+
+    public String getNameFolder() {
+        return nameFolder;
+    }
+
+    public void setNameFolder(String nameFolder) {
+        this.nameFolder = nameFolder;
+    }
+}
---
+++ app/src/main/java/com/lapvt/myapplication/recyclerviewb3/RecyclerViewB3Activity.java
@@ -0,0 +1,16 @@
+package com.lapvt.myapplication.recyclerviewb3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lapvt.myapplication.R;
+
+public class RecyclerViewB3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_b3);
+    }
+}
