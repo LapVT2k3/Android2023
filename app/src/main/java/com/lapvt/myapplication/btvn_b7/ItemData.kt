1244f473b672660912e3859ddcac1429797daa5f
LapVT2k3 <kendyvu2k3@gmail.com>
2023-09-11 09:43:36 +0700

    b7

--- app/src/main/java/com/lapvt/myapplication/btvn_b7/ItemData.kt
+++ app/src/main/java/com/lapvt/myapplication/btvn_b7/ItemData.kt
@@ -1,6 +1,5 @@
 package com.lapvt.myapplication.btvn_b7
 
-//data class ItemData() {
-//    var title: String = ""
-//    var content: String = ""
-//}
+data class ItemData(val image: Int, val title: String, val content: String) {
+
+}
