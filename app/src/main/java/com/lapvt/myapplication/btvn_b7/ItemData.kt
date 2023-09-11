4053211615e32e5d5caadcc0a26a04c845140ed5
LapVT2k3 <kendyvu2k3@gmail.com>
2023-09-11 09:36:35 +0700

    b7

---
+++ app/src/main/java/com/lapvt/myapplication/btvn_b7/ItemData.kt
@@ -0,0 +1,6 @@
+package com.lapvt.myapplication.btvn_b7
+
+//data class ItemData() {
+//    var title: String = ""
+//    var content: String = ""
+//}
---
+++ app/src/main/java/com/lapvt/myapplication/btvn_b7/Screen1Activity.kt
@@ -0,0 +1,14 @@
+package com.lapvt.myapplication.btvn_b7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lapvt.myapplication.R
+
+class Screen1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen1)
+
+
+    }
+}
