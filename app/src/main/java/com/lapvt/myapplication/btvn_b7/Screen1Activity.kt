878ccb6f03f11ee06feb25b505c3d8121ae24f16
LapVT2k3 <kendyvu2k3@gmail.com>
2023-09-14 17:21:09 +0700

    btvn_b7

--- app/src/main/java/com/lapvt/myapplication/btvn_b7/Screen1Activity.kt
+++ app/src/main/java/com/lapvt/myapplication/btvn_b7/Screen1Activity.kt
@@ -14,20 +14,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.lapvt.myapplication.R
 
 class Screen1Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen1)
 
+        var messageList: ArrayList<MessageData>? = null
         val ivBack: ImageView = findViewById(R.id.ivBack)
         val tvAdd: TextView = findViewById(R.id.tvSave)
-
-        val startForResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    val message = result.data?.extras?.get("key1") as? MessageData
-
-                }
-            }
+        messageList = createMessageList()
 
         ivBack.setOnClickListener {
             Toast.makeText(this, "Thoát khỏi ứng dụng", Toast.LENGTH_SHORT).show()
@@ -40,16 +35,29 @@ class Screen1Activity : AppCompatActivity() {
 
         val rcvData: RecyclerView = findViewById(R.id.rcvData)
         rcvData.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        val adapter = MessageAdapter(createMessageList())
+
+        val adapter = messageList?.let { MessageAdapter(it) }
         rcvData.adapter = adapter
 
-        adapter.onClickItem = {
+        adapter?.onClickItem = {
 
         }
 
+        val startForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val message = result.data?.extras?.get("key1") as? MessageData
+                    val i = messageList?.size
+                    if (message != null) {
+                        i?.let { messageList?.add(it, message) }
+                        i?.let { adapter?.notifyItemInserted(it) }
+                    }
+                }
+            }
+
     }
 
-    public fun createMessageList(): ArrayList<MessageData> {
+    public fun createMessageList(): ArrayList<MessageData>? {
         val messageList: ArrayList<MessageData> = ArrayList()
         messageList.add(MessageData(R.drawable.ic_message, "Tổng hợp tin tức thời sự", "Tổng hợp tin tức thời sự nóng hổi nhất, của tất cả các báo nổi nhất hiện nay"))
         messageList.add(MessageData(R.drawable.ic_message, "Do It Your Self", "Sơn tùng MTP quá đẹp trai hát hay"))
